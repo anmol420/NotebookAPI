832818e52662bd78e1182182f16d48c494134c81
anmol420 <anmolanand2k15@gmail.com>
2025-06-28 22:09:13 +0530

    Initial Commit

---
+++ src/main/java/com/anmol420/notebook_api/NotebookApiApplication.java
@@ -0,0 +1,13 @@
+package com.anmol420.notebook_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotebookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotebookApiApplication.class, args);
+	}
+
+}
