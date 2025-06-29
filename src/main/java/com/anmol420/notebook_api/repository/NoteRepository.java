08caa5833b06a1f7824a4cbf64da08e3c606ba07
anmol420 <anmolanand2k15@gmail.com>
2025-06-29 10:01:27 +0530

    Added Repository

---
+++ src/main/java/com/anmol420/notebook_api/repository/NoteRepository.java
@@ -0,0 +1,11 @@
+package com.anmol420.notebook_api.repository;
+
+import com.anmol420.notebook_api.domain.entities.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note, UUID> {
+}
