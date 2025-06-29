e52ec4dd91e28ffdb4b2d5aea86fbea302bf215b
anmol420 <anmolanand2k15@gmail.com>
2025-06-29 10:47:16 +0530

    Added User Security Layer

---
+++ src/main/java/com/anmol420/notebook_api/security/JwtUserDetails.java
@@ -0,0 +1,58 @@
+package com.anmol420.notebook_api.security;
+
+import com.anmol420.notebook_api.domain.entities.User;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@RequiredArgsConstructor
+public class JwtUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    public UUID getId() {
+        return user.getId();
+    }
+}
---
+++ src/main/java/com/anmol420/notebook_api/security/JwtUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.anmol420.notebook_api.security;
+
+import com.anmol420.notebook_api.domain.entities.User;
+import com.anmol420.notebook_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+
+        return new JwtUserDetails(user);
+    }
+}
