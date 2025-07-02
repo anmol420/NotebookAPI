556a5f572f3b01193cad8a3944b857b892054e59
anmol420 <anmolanand2k15@gmail.com>
2025-07-02 20:21:05 +0530

    Added Bug Fix

--- src/main/java/com/anmol420/notebook_api/security/JwtUserDetails.java
+++ src/main/java/com/anmol420/notebook_api/security/JwtUserDetails.java
@@ -34,22 +34,22 @@ public class JwtUserDetails implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 
     public UUID getId() {
